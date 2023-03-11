a68c0d3bc0fb06a6235a413ad4d5482fb431a7ef
Adam Gragnic <adamgragnic@gmail.com>
2023-03-11 21:25:48 +0100

    Ajout du routeur

---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NotFound() {
+    return (
+        <div>
+            Page non trouver. <a href="/">Retourner sur l'accueil</a>
+        </div>
+    );
+}
+
+export default NotFound;
